9dfcb257ae783c1c4959fb0f063167cd284efb82
sadoway7 <143480868+sadoway7@users.noreply.github.com>
2025-05-02 12:57:44 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,13 +8,13 @@ export default defineConfig({
     host: '0.0.0.0', // Correctly allow your domain
     port: 4173,
     strictPort: true,
-    allowedHosts: ['test.sadoway.ca', 'cc2.sadoway.ca'],
+    allowedHosts: ['test.sadoway.ca', 'cc2.sadoway.ca', 'www.ceramicscanada.net'],
   },
   preview: {
     host: '0.0.0.0', // Correctly allow your domain for preview
     port: 4173, // Use the same port as the dev server
     strictPort: true,
-    allowedHosts: ['test.sadoway.ca', 'cc2.sadoway.ca'],
+    allowedHosts: ['test.sadoway.ca', 'cc2.sadoway.ca', 'www.ceramicscanada.net'],
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
